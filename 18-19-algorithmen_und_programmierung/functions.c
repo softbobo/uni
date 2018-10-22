55a49bd3285ed9cfe8c65ff7847656445d3dc0af
Robert Schulze <robert_schulze@posteo.de>
2018-10-22 20:52:35 +0200

    added functiontest.c and functions.c for next task, workink vers tbc

---
+++ 18-19-algorithmen_und_programmierung/functions.c
@@ -0,0 +1,29 @@
+//implements the functions necessary for task 005
+
+//sum of all numbers 1 to n
+
+unsigned int sum(unsigned int n)
+{
+  if(n != 0)
+  return n + sum(n-1);
+  else return n;
+}
+
+//sum of first odd numbers in n
+unsigned int sum0dd(unsigned int n)
+{
+
+}
+
+
+//number of digits of n
+unsigned int digits(unsigned int n)
+{
+
+}
+
+//checksum of n
+unsigned int checksum(unsigned int n)
+{
+
+}
--- 18-19-algorithmen_und_programmierung/functiontest.c
+++ 18-19-algorithmen_und_programmierung/functiontest.c
@@ -4,8 +4,12 @@
 
 //function declaration - aka function call in python
 unsigned int sum(unsigned int n)
+unsigned int sum0dd(unsigned int n)
+unsigned int digits(unsigned int n)
+unsigned int checksum(unsigned int n)
+
 
 int main()
 {
-  printf("sum(%d) = %d\n", 3, sum;
+  printf("sum(%d) = %d\n", 3, sum(3));
 }
