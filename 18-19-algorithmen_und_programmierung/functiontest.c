45e90deb9e8e24487b73294db09f67d7d7b592ec
Robert Schulze <robert_schulze@posteo.de>
2018-10-22 20:12:07 +0200

    added file functiontest.c as test utility - task 005

---
+++ 18-19-algorithmen_und_programmierung/functiontest.c
@@ -0,0 +1,11 @@
+//simple main program to test functions.c
+
+#include <stdio.h>
+
+//function declaration - aka function call in python
+unsigned int sum(unsigned int n)
+
+int main()
+{
+  printf("sum(%d) = %d\n", 3, sum;
+}
