0ba70fcfb774bdff471d30aed9983a006000bd5e
Robert Schulze <robert_schulze@posteo.de>
2018-10-23 16:25:58 +0200

    second commit for stuff for task 005 + cpp stuff for tutorial

--- 18-19-algorithmen_und_programmierung/functions.c
+++ 18-19-algorithmen_und_programmierung/functions.c
@@ -1,7 +1,6 @@
 //implements the functions necessary for task 005
 
 //sum of all numbers 1 to n
-
 unsigned int sum(unsigned int n)
 {
   if(n != 0)
@@ -12,18 +11,35 @@ unsigned int sum(unsigned int n)
 //sum of first odd numbers in n
 unsigned int sum0dd(unsigned int n)
 {
-
+  if(n != 0 && n % 2 == 1)
+  return n + sum0dd(n-1);
+  else if(n != 0 && n % 2 == 0)
+  return n-1;
+  else return n;
 }
 
 
 //number of digits of n
 unsigned int digits(unsigned int n)
 {
-
+  unsigned int count = 0;
+  while (n != 0)
+  {
+  n /= 10;
+  ++count;
+  
+  }
+  return count;
 }
 
 //checksum of n
 unsigned int checksum(unsigned int n)
 {
-
+  int check = 0;
+  while(n != 0)
+  {
+    check += n % 10;
+    n /= 10;
+  }
+  return check;
 }
--- 18-19-algorithmen_und_programmierung/functiontest.c
+++ 18-19-algorithmen_und_programmierung/functiontest.c
@@ -3,13 +3,19 @@
 #include <stdio.h>
 
 //function declaration - aka function call in python
-unsigned int sum(unsigned int n)
-unsigned int sum0dd(unsigned int n)
-unsigned int digits(unsigned int n)
-unsigned int checksum(unsigned int n)
+//unsigned int sum(unsigned int n);
+//unsigned int sum0dd(unsigned int n);
+//unsigned int digits(unsigned int n);
+unsigned int checksum(unsigned int n);
 
 
 int main()
 {
-  printf("sum(%d) = %d\n", 3, sum(3));
+  //unsigned int n;
+  //printf("Please enter a positive integer: ");
+  //scanf('%d', &n);                              //scanf causes errors atm b/c of pointers
+  //printf("sum(%d) = %d\n", n, sum(n));
+  //printf("sum0dd(%d)= %d\n", '3', sum0dd(3));
+  //printf("digits(%d) = %d\n", "24", digits(24));
+  printf("checksum(%d) = %d\n", "24", checksum(24));
 }
