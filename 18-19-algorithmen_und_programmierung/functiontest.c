05511fa66538258d04c17130687f9d50610a8399
Robert Schulze <robert_schulze@posteo.de>
2018-11-03 16:50:27 +0100

    added working version of sqrt.c

--- 18-19-algorithmen_und_programmierung/functiontest.c
+++ 18-19-algorithmen_und_programmierung/functiontest.c
@@ -1,4 +1,4 @@
-//simple main program to test functions.c
+//simple main program to test functions
 
 #include <stdio.h>
 
@@ -6,16 +6,21 @@
 //unsigned int sum(unsigned int n);
 //unsigned int sum0dd(unsigned int n);
 //unsigned int digits(unsigned int n);
-unsigned int checksum(unsigned int n);
-
+//unsigned int checksum(unsigned int n);
+unsigned long int squareroot (unsigned long int val);
 
 int main()
 {
+  unsigned long int val = 16;
   //unsigned int n;
   //printf("Please enter a positive integer: ");
   //scanf('%d', &n);                              //scanf causes errors atm b/c of pointers
   //printf("sum(%d) = %d\n", n, sum(n));
   //printf("sum0dd(%d)= %d\n", '3', sum0dd(3));
   //printf("digits(%d) = %d\n", "24", digits(24));
-  printf("checksum(%d) = %d\n", "24", checksum(24));
-}
+  //printf("checksum(%d) = %d\n", "24", checksum(24));
+  printf("The squareroot of %d is %d\n", val, squareroot(val));
+  return 0;
+  
+  return 0;
+  }
---
+++ 18-19-algorithmen_und_programmierung/sqrt.c
@@ -0,0 +1,33 @@
+//heron test
+#include <stdio.h>
+
+unsigned long int squareroot(unsigned long int val);
+unsigned long int helproot();
+
+int main(){
+    printf("Die Wurzel aus %lu ist %lu. \n", 
+    (unsigned long)174897, squareroot((unsigned long)174897));
+    return 0;
+}
+
+unsigned long int squareroot(unsigned long int val)
+{
+    //unsigned long *pval = &val;
+
+    if(val == 0)  
+    return 0;
+    
+    else return helproot(1, val);
+}
+
+
+unsigned long int helproot(unsigned long int x, unsigned long y)
+{
+    x = (x+(y/x))/2;
+    
+    if(x*x<=y)
+    return x;
+
+
+    else return helproot(x, y);
+}
