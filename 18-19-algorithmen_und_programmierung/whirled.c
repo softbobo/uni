f0b7a6d18d08a09e88bba0da5bb19e35b31b82ad
Robert Schulze <robert_schulze@posteo.de>
2018-11-14 08:00:30 +0100

    intitial v of whirled - does not function

--- 18-19-algorithmen_und_programmierung/whirled.c
+++ 18-19-algorithmen_und_programmierung/whirled.c
@@ -3,69 +3,34 @@
 int printf(const char * restrict, ...);
 void *malloc(unsigned long);
 
-const char *whirled(const char * const str); // Deklaration der Aufgabenfunktion
-char taz(const char * const nltrs); /*function that substitutes all letters
-with their counterparts from the opposite side of the alphabets*/
-char length(const char *p_str[]);
+const char *whirled(const char * const str); 
+unsigned long long input_length(const char * const p_str, unsigned long long length);
 
-int main(int carg, const char **varg)
-{
-    if (carg != 2) return -1; // error, sofern mehr als ein Parameter (zusätzlich zum Programmnamen), übergeben wird
-    printf("Die verwürfelte Version von \"%s\" ist \"%s\".\n", varg[1], whirled(varg[1]));
-    //gibt ergebnis aus
-    
-    return 0; // 0 bedeutet alles okay
+
+int main(int carg, const char **varg){
+
+    if (carg != 2) return -1; 
+    printf("Die verwürfelte Version von \"%s\" ist \"%s\".\n", 
+    varg[1],whirled(varg[1]));
+    return 0; 
 }
-
-
-//Mein Code ab hier
 
 const char *whirled(const char * const str)
+{
+    unsigned long long str_length = input_length(str, 0);
+    printf("kontrolle. eingegebener string: \"%s\"; 
+    länge des strings: \"%lld\"\n", str, str_length);
+    //kontroll-ausgabe -> ist string korrekt übergeben worden und stimmt seine länge?
+    return 0;
+}
+
+unsigned long long input_length(const char * const p_str, unsigned long long length)
 
 {
-    unsigned long int strlen;
-    strlen = length(str);
-    printf("kontrolle:\"%s\", arraylänge: \"%d\"\n", str, sizeof(strlen)); //vor abgabe löschen
+    // SEG FAULT DA VERMUTLICH ZEIGER FALSCH INITIALISIERT
+    //const char * copy_str = p_str;
+    if(*p_str != '\0')
+    return  input_length(p_str, length+1);
+    else return  length;
 
-    //taz_str = taz(str[])
-
-
-    
-    //for reversion i need two pointers, one pointing to the first position
-    //second to the last position of the array, that holds the string
-    //the length of the string is stored in carg, so the last position
-    //should be equal to the value of carg
-    //i also need an array, that holds the inverted string
-
-    
-    
-    
-   
-    //second invert charstring
-
-    //third declare all char substitutions
-    //substitution via a struct that holds all the subtitution pairs
-
-    
-    //probably have to do this char for char
-    //fourth print out whole string
-
-return str;
 }
-const char length(const char *p_str[])
-{   
-    printf("kontrolle: \"%p\"\n", p_str); //vor abgabe auskommentieren
-    //unsigned long long length[];
-    //if (length <= p_str)
-    //return length+1;
-    //else
-     return (unsigned long long) length;
-}
-
-/*struct subs
-    {
-        char old;
-        char new;
-    } ltr = {A,B,C,D,E,F,G,I,J,K,L,M,N,O,P,
-    Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,
-    k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}*/
