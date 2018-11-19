4ad1315f577b111c2f8a6be9376c2ec7b296e656
Robert Schulze <robert_schulze@posteo.de>
2018-11-19 20:30:22 +0100

    first vers of main() outputs all single chars handed over as debug

--- 18-19-algorithmen_und_programmierung/prec/prec.c
+++ 18-19-algorithmen_und_programmierung/prec/prec.c
@@ -1,5 +1,25 @@
 //AuP 4th task winter semester 2018/19, written by softbobo
 //find task in README.md
 
+/* tasks:
+-configure debugger 
+*/
+
 #include "stdaup.h"
 
+int main(int argc, char **argv){
+
+    /*main() function takes arguments, calls functions to do 'sub-algorithms'
+    and outputs result*/
+
+    if(argc<0) return -1;               //check, if sufficient number of args are given
+    
+    long long i;
+    for(i=0; i<=argc; i++){
+        //debug-statement: all args handed over?
+        printf(" args count: %d, input: \"%s\"\n", argc, argv[i]);                                 
+    }
+    
+
+    return 0;
+}
