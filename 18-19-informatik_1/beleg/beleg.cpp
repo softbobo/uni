e133cf56abcbfa55305e301247b426131de88c57
Robert Schulze <robert_schulze@posteo.de>
2018-12-09 21:41:59 +0100

    initial commit for appointment in informatik 1

--- 18-19-informatik_1/beleg/beleg.cpp
+++ 18-19-informatik_1/beleg/beleg.cpp
@@ -6,7 +6,27 @@ Student: Robert Schulze, Matrikelnummer: 555625 */
 #include<iostream>
 using namespace std;
 
+struct fliese{
+    float x;  /* Breite (x-Richtung) in cm */
+    float y;  /* Laenge (y-Richtung) in cm */
+};
+
 int main(void){
 
+    float size_a, size_b;                       //wallsize in m
+    cout << "Bitte die Breite des Raumes in Metern eingeben: " << endl;
+    cin >> size_a;
+    cout << "Bitte die Laenge des Raumes in Metern eingeben: " << endl;
+    cin >> size_b;
+    cout << "Bitte die Breite der Fliesen in Centimetern eingeben: " << endl;
+    cin >> x;
+    cout << "Bitte die Laenge der Fliesen in Centimetern eingeben: " << endl;
+    cin >> y;
 
+    cout << "Kontrolle: Raumbreite: " << size_a << " Raumlaenge: " << size_b << endl;
+    cout << "Fliesenbreite: " << x << " Fliesenlaenge: " << y << endl; 
+    //fliese wand[size_a][size_b]                 //wall-simulation - array holds all the structs representing tiles
+
+
+    return 0;
 }
