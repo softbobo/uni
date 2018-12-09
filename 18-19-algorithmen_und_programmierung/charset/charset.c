2e6c1ccdaedffb3bc339b7a19284049fb1d00477
Robert Schulze <robert_schulze@posteo.de>
2018-12-09 16:37:46 +0100

    initial version for first function modified

--- 18-19-algorithmen_und_programmierung/charset/charset.c
+++ 18-19-algorithmen_und_programmierung/charset/charset.c
@@ -11,10 +11,28 @@ charset_t *charset_new(const char* elements){
 
     charset_t chars = {};
 
-    for(int i = 0; i <= 51; i++)
-        for(int j = 0; elements[j] != '\0'; j++)
+    for(int i = 0; i <= 51; i++)                //test for every single letter of alphabet[]
+        for(int j = 0; elements[j] != '\0'; j++)//iterate through whole input string
         {
-            if(elements[j])
+            if(elements[j] == alphabet[i])      //if a letter is found shift chars[] 'one' bit and break
+            {                                   //if it isn't present shift 'zero' bit
+                chars >> 1;                     
+                break;
+            }
+            else
+                chars >> 0;
         }
+    return charset chars;
 
 }
+
+/* hints:
+- shifting algorithm for whole array https://stackoverflow.com/questions/10367616/bitwise-shifting-array-of-chars
+- implementation of bit array (useful!) http://www.mathcs.emory.edu/~cheung/Courses/255/Syllabus/1-C-intro/bit-array.html
+- implementation of bit array https://www.codeproject.com/Tips/53433/BitArray-in-C
+- library for bit operations https://www.jjj.de/bitwizardry/bitwizardrypage.html
+- lib function to compare two strings (for task 2) http://pubs.opengroup.org/onlinepubs/9699919799/functions/strspn.html
+- bit twiddling hacks http://graphics.stanford.edu/~seander/bithacks.html
+- collection of low lever algorithms http://aggregate.org/MAGIC/
+- the bit twiddler https://bits.stephan-brumme.com/
+
