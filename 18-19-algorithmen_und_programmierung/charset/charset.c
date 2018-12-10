c7287b93ec41f38f4ea99ac73470e240f83e1932
Robert Schulze <robert_schulze@posteo.de>
2018-12-10 16:36:23 +0100

    version of first function still throws various errors

--- 18-19-algorithmen_und_programmierung/charset/charset.c
+++ 18-19-algorithmen_und_programmierung/charset/charset.c
@@ -1,29 +1,55 @@
 #include "stdaup.h"
 #include "charset.h"
 
+unsigned long long input_length(const char* input, unsigned long long count);
+
 charset_t *charset_new(const char* elements){
 //function tests for A-Z and a-z if char is present in 'elements' string
 
-    char* alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-    'x', 'y', 'z'};
+    const char alphabet[53] = 
+    {122, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97,
+    90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65};
+    /*{'z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h',
+    'g','f','e','d','c','b','a','Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K',
+    'J','I','H','G','F','E','D','C','B','A'};*/
+    //lookup table for alphabet is in reversed order, since chars[] is filled up beginning with the 
+    //LSB of the last byte
 
-    charset_t chars = {};
+    charset_t chars[] = malloc(sizeof(alphabet)); //FREE THIS LATER!
+    char *p_chars;
+    p_chars = chars;                            //pointer for value-return
+    unsigned long long len = input_length(elements, 0); 
 
     for(int i = 0; i <= 51; i++)                //test for every single letter of alphabet[]
-        for(int j = 0; elements[j] != '\0'; j++)//iterate through whole input string
+        {
+        int byte_pos = (i/8) + (7-i/8);         //perform bitshift for the right byte of the array, beginning from the last
+        printf("current byte: %d", byte_pos);   //debug-print
+        for(int j = 0; elements[j] <= len; j++)//iterate through whole input string
         {
             if(elements[j] == alphabet[i])      //if a letter is found shift chars[] 'one' bit and break
             {                                   //if it isn't present shift 'zero' bit
-                chars >> 1;                     
+                chars[byte_pos] >>= 1; 
+                printf("Character added: %c, must be equivalent to alphabet: %c\n", elements[j], alphabet[i]);
+                //debug-statement          
                 break;
             }
-            else
-                chars >> 0;
-        }
-    return charset chars;
+            else if(elements[j] == '\0')
+                chars[byte_pos] >>= 0;           //fill a 0 if char isn't present
+                printf("current pos in alphabet: %c, added a 0\n", alphabet[i]);
+                //debug-statement
+            }
+    }
+    return p_chars;
 
+}
+
+unsigned long long input_length(const char* input, unsigned long long count)
+//determines length of input string including terminating '/0'
+{
+    for(unsigned long long i = 0; input[i] != '\0'; i++)
+        count++;
+
+    return count += 1;
 }
 
 /* hints:
@@ -35,4 +61,6 @@ charset_t *charset_new(const char* elements){
 - bit twiddling hacks http://graphics.stanford.edu/~seander/bithacks.html
 - collection of low lever algorithms http://aggregate.org/MAGIC/
 - the bit twiddler https://bits.stephan-brumme.com/
+*/
+
 
--- 18-19-algorithmen_und_programmierung/charset/charset_test.c
+++ 18-19-algorithmen_und_programmierung/charset/charset_test.c
@@ -1,2 +1,20 @@
 //test-file for functions in charset.c, not to be included later
 
+#include<stdio.h>
+#include"charset.h"
+#include"stdaup.h"
+
+
+int main()
+{
+    const char str_1[] = "ABCabc";
+    const char str_2[] = "fg hj 1236 lm";
+    const char str_3[] = "AAAAAA bbbb 678 888 ß §";
+
+    //tests for func charset_new
+    printf("Function charset_new String 1: %s \n", charset_new(str_1));
+    printf("Function charset_new String 2: %s \n", charset_new(str_2));
+    printf("Function charset_new String 3: %s \n", charset_new(str_3));
+
+    return 0;
+}
