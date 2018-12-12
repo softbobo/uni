6e5f573f404cf732c803f5db55a1e1a31497b804
Robert Schulze <robert_schulze@posteo.de>
2018-12-12 13:43:13 +0100

    inner oop of charset.c doesnt get entered

--- 18-19-algorithmen_und_programmierung/charset/charset.c
+++ 18-19-algorithmen_und_programmierung/charset/charset.c
@@ -15,31 +15,37 @@ charset_t *charset_new(const char* elements){
     //lookup table for alphabet is in reversed order, since chars[] is filled up beginning with the 
     //LSB of the last byte
 
-    charset_t chars[] = malloc(sizeof(alphabet)); //FREE THIS LATER!
-    char *p_chars;
-    p_chars = chars;                            //pointer for value-return
-    unsigned long long len = input_length(elements, 0); 
-
+    struct charset *chars;
+    chars = malloc(sizeof(struct charset));
+    //for(int i = 0; i <= 6; i++)                 //initialize whole bits[] to 0
+    //    chars->bits[i] = 0; 
+    int len = input_length(elements, 0);
+    printf("length of input string is: %d\n", len);
+    printf("input string is: %s\n", elements);
+    
     for(int i = 0; i <= 51; i++)                //test for every single letter of alphabet[]
         {
-        int byte_pos = (i/8) + (7-i/8);         //perform bitshift for the right byte of the array, beginning from the last
-        printf("current byte: %d", byte_pos);   //debug-print
-        for(int j = 0; elements[j] <= len; j++)//iterate through whole input string
+        int byte_pos = (i/8);                   //perform bitshift for the right byte of the array, beginning from the last
+        printf("current byte: %d, letter: %c\n", byte_pos, alphabet[i]); //debug-print
+        for(int j = 0; elements[j] <= len; j++) //iterate through whole input string
         {
-            if(elements[j] == alphabet[i])      //if a letter is found shift chars[] 'one' bit and break
-            {                                   //if it isn't present shift 'zero' bit
-                chars[byte_pos] >>= 1; 
+            printf("position in input string: %c", elements[j]); //debug-print
+            if(elements[j] == alphabet[i])      //if a letter is found shift chars[] 'one' bit to th right and break
+            {                                   
+                chars->bits[byte_pos] >>= 1; 
                 printf("Character added: %c, must be equivalent to alphabet: %c\n", elements[j], alphabet[i]);
                 //debug-statement          
                 break;
             }
             else if(elements[j] == '\0')
-                chars[byte_pos] >>= 0;           //fill a 0 if char isn't present
+            {
+                chars->bits[byte_pos] >>= 0;           //fill a 0 if char isn't present
                 printf("current pos in alphabet: %c, added a 0\n", alphabet[i]);
                 //debug-statement
             }
+        }
     }
-    return p_chars;
+    return chars;
 
 }
 
