363adb4d6d7d3f8869e4a4119db5439c5e2fa994
Robert Schulze <robert_schulze@posteo.de>
2018-12-15 17:24:45 +0100

    working version for task 3, others still to be finetuned

--- 18-19-algorithmen_und_programmierung/charset/charset.c
+++ 18-19-algorithmen_und_programmierung/charset/charset.c
@@ -2,10 +2,19 @@
 #include "charset.h"
 
 unsigned long long input_length(const char* input, unsigned long long count);
+//helper for task 1
 
-charset_t *charset_new(const char* elements){
-//function tests for A-Z and a-z if char is present in 'elements' string
+charset_t* union_func(charset_t* s1, const charset_t* s2);
+charset_t* cut_func(charset_t* s1, const charset_t* s2);
+charset_t* symdiff_func(charset_t* s1, const charset_t* s2);
+//helpers for task 2
 
+charset_t *charset_new(const char* elements)
+{
+//for task 1
+//still to do:
+//populate output in correct order
+//implement correct test cases in charset_test
     const char alphabet[53] = 
     {122, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97,
     90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65};
@@ -17,8 +26,6 @@ charset_t *charset_new(const char* elements){
 
     struct charset *chars;
     chars = malloc(sizeof(struct charset));
-    //for(int i = 0; i <= 6; i++)                 //initialize whole bits[] to 0
-    //    chars->bits[i] = 0; 
     int len = input_length(elements, 0);
     printf("length of input string is: %d\n", len);
     printf("input string is: %s\n", elements);
@@ -27,9 +34,9 @@ charset_t *charset_new(const char* elements){
         {
         int byte_pos = (i/8);                   //perform bitshift for the right byte of the array, beginning from the last
         printf("current byte: %d, letter: %c\n", byte_pos, alphabet[i]); //debug-print
-        for(int j = 0; elements[j] <= len; j++) //iterate through whole input string
+        for(int j = 0; j <= len; j++) //iterate through whole input string
         {
-            printf("position in input string: %c", elements[j]); //debug-print
+            printf("position in input string: %c\n", elements[j]); //debug-print
             if(elements[j] == alphabet[i])      //if a letter is found shift chars[] 'one' bit to th right and break
             {                                   
                 chars->bits[byte_pos] >>= 1; 
@@ -50,12 +57,104 @@ charset_t *charset_new(const char* elements){
 }
 
 unsigned long long input_length(const char* input, unsigned long long count)
-//determines length of input string including terminating '/0'
+//determines length of input string in task 1 including terminating '\0'
 {
     for(unsigned long long i = 0; input[i] != '\0'; i++)
         count++;
 
-    return count += 1;
+    return count;
+}
+
+int charset_op(charset_t* s1, const charset_t* s2, csopt_t op)
+{
+//for task 2
+    printf("string 1: %s, string 2: %s\n", s1, s2);     //debug
+
+    if(s1 == NULL || s2 == NULL)            //check for Nullpointers
+        return -1;
+    else
+    {
+        switch(op)                          //switch for given enum constants, store result in s1
+        {                                   
+            case CS_UNION:      union_func(s1, s2);
+                                break;
+            case CS_CUT:        cut_func(s1, s2);
+                                break;
+            case CS_SYMDIFF:    symdiff_func(s1, s2);
+                                break;
+            default:            return -1;
+        }
+    }
+    return 0; 
+}
+
+charset_t* union_func(charset_t* s1, const charset_t* s2)
+{
+//calculates union of both operands
+
+    for(int i = 0; i <= 6; i++)
+    {
+        s1->bits[i] &= s2->bits[i];
+    }
+    return s1;
+}
+
+charset_t* cut_func(charset_t* s1, const charset_t* s2)
+{
+//calculates intersection of both operands
+    for(int i = 0; i <= 6; i++)
+    {
+        s1->bits[i] |= s2->bits[i];
+    }
+    return s1;
+}
+
+charset_t* symdiff_func(charset_t* s1, const charset_t* s2)
+{
+//calculates intersection of both operands
+    for(int i = 0; i <= 6; i++)
+    {
+        s1->bits[i] ^= s2->bits[i];
+    }
+    return s1;
+}
+
+char *charset_tos(const charset_t* s)
+{
+/*function for task 3, searches input str for chars, populates output str with found chars
+alphabetically, capitals first*/
+
+    static char output[8] = {0};
+
+    char lookup[53] = {0};
+
+    int i, j;
+    for(i = 0, j = 65; i < 52; i += 2)    //populates array w/ capitals
+    {
+        lookup[i] = j;
+        printf("char added: %c , pos: %d\n", j, i);
+        j++;
+    }
+    for(i = 1, j = 97; i < 52; i += 2)    //populates array w/ small letters
+    {
+        lookup[i] = j;
+        printf("char added: %c , pos: %d\n", j, i);
+        j++;
+    }
+    int l = 0;
+    for(i = 0; lookup[i] != 0; i++)
+    {
+        for(j = 0; s->bits[j] != 0; j++)
+        {
+            if(lookup[i] == s->bits[j])
+            {
+                output[l] = s->bits[j];
+                printf("position in output: %d, char added to output: %c\n",l , output[l]);
+                l++;
+            }
+        }
+    }
+    return output;
 }
 
 /* hints:
--- 18-19-algorithmen_und_programmierung/charset/charset_test.c
+++ 18-19-algorithmen_und_programmierung/charset/charset_test.c
@@ -7,14 +7,38 @@
 
 int main()
 {
-    const char str_1[] = "ABCabc";
-    const char str_2[] = "fg hj 1236 lm";
-    const char str_3[] = "AAAAAA bbbb 678 888 ß §";
-
     //tests for func charset_new
-    printf("Function charset_new String 1: %s \n", charset_new(str_1));
-    printf("Function charset_new String 2: %s \n", charset_new(str_2));
-    printf("Function charset_new String 3: %s \n", charset_new(str_3));
+
+    //const char str_11[] = "ABCabc";
+    //const char str_12[] = "fg hj 1236 lm";
+    //const char str_13[] = "AAAAAA bbbb 678 888 ß §";
+
+    //struct charset *temp_str;
+    //temp_str = malloc(sizeof(struct charset));
+    //temp_str = charset_new(str_11);
+    //printf("Function charset_new, String 1: %s \n", temp_str->bits);
+    //printf("Function charset_new String 2: %s \n", charset_new(str_2));
+    //printf("Function charset_new String 3: %s \n", charset_new(str_3));
+//
+    ////tests for charset op
+//
+    //charset_t* str_21;
+    //str_21.bits -> {1};
+    //charset_t* str_22;
+    //str_22.bits -> {1};
+    ////charset_t* str_23;
+    ////str_23.bits -> {NULL};
+//
+    //int result_2a = charset_op(str_21, str_22, CS_UNION);
+    //int result_2b = charset_op(str_21, str_22, CS_CUT);
+    //int result_2c = charset_op(str_21, str_22, CS_SYMDIFF);
+//
+    //printf("success union: %d, success cut: %d, success symdiff: %d\n", result_2a, result_2b, result_2c);
+
+    //tests for charset_tos
+    charset_t* str_31 = "AdBCe";
+
+    printf("string ordered: %s\n", charset_tos(str_31));
 
     return 0;
 }
