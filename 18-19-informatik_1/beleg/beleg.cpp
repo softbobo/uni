fcdfb05ca16fd3ebf30501cdb28a08dbf9d00de3
Robert Schulze <robert_schulze@posteo.de>
2019-01-10 14:23:27 +0100

    added array allocation and printing functions, works only for squared tiles as of now

--- 18-19-informatik_1/beleg/beleg.cpp
+++ 18-19-informatik_1/beleg/beleg.cpp
@@ -6,7 +6,10 @@ Student: Robert Schulze, Matrikelnummer: 555625 */
 /* next steps:
 - write check if temp_y is an int multiple of temp_x in tile_in()
 - write array_allocator(), that takes args - wall and tile - from main() and returns an array
+- how to allocate array correctly and pass correctly?
 - write array_printer(), that takes array from main() and prints array to console 
+- how to determine ends of each "line of tiles" (aka 2nd array dimension)
+- remove all debug printouts
 */
 
 #include<iostream>
@@ -19,18 +22,37 @@ struct fliese{
 
 struct fliese* tile_in(struct fliese *p_tile);
 struct fliese* wall_in(struct fliese *p_wall);
+struct fliese** array_allocator(struct fliese* p_tile, struct fliese* p_wall, struct fliese** raum, long long size);
+void array_printer(struct fliese** raum, long long size);
 
 int main(void){
+    
     struct fliese *p_tile = new fliese;
     tile_in(p_tile);
     cout << "Debug-Output. Fliesenbreite: " << p_tile->x << " Fliesenlaenge: " << p_tile->y << endl; //delete before handing in
+    
     struct fliese *p_wall = new fliese;
     wall_in(p_wall);
-    cout << "Debug-Output. Wandbreite: " << p_wall->x << " Wandhoehe: " << p_wall->y << endl;  //delete before handing in
+    cout << "Debug-Output. Wandbreite: " << p_wall->x << " Wandhoehe: " << p_wall->y << endl;   //delete before handing in
+    
+    const long long rows = (p_wall->x/p_tile->x);
+    const long long cols = (p_wall->y/p_tile->y);
+    struct fliese** raum = new struct fliese*[rows];
+    for(int i = 0; i <= rows; i++) {
+        raum[i] = new fliese[cols];
+    }
+    cout << "Debug-Output. Struct-Größe in Byte " << sizeof(fliese) << endl;                    //delete befor handing in
+    cout << "Debug-Output. Array-Größe in Byte " << sizeof(raum) << endl;                       //delete before handing in
+    
+    array_allocator(p_tile, p_wall, raum, (p_wall->y/p_tile->y));
+    
+    cout << "Der Verlegeplan für die eingegebenen Masze sieht wie folgt aus: " << endl;
+    array_printer(raum, (p_wall->y/p_tile->y));
 
 
     free(p_tile);
-    free(p_wall);         
+    free(p_wall);
+    free(raum);         
     return 0;
 }
 
@@ -84,4 +106,28 @@ struct fliese* wall_in(struct fliese *p_wall) {
             p_wall->y = wall_height; 
         }
     return p_wall;
+}
+
+struct fliese** array_allocator(struct fliese* p_tile, struct fliese* p_wall,  struct fliese** raum, long long size) {
+    if(p_tile->x == p_tile->y) {
+        //fill array if tile length and height are equal
+        for(long long i = 0; i <= (p_wall->x / p_tile->x); i++) {
+            cout << "allocated " << i << " line" << endl;
+            for(long long j = 0; j <= size; j++) {
+                raum[i][j].x = 1;
+                raum[i][j].y = 1;
+            }
+        }
+    }
+
+    return raum;
+}
+
+void array_printer(struct fliese** raum, long long size) {
+    for(long long i = 0; raum[i]; i++) {
+            for(long long j = 0; j <= size ; j++) {
+                cout << raum[i][j].x << " " << raum[i][j].y << " | ";
+            }
+        cout << endl;
+        }
 }
