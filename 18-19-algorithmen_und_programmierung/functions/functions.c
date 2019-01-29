b9b93d13d8a212541fa03650d30cd2bb004c309e
Robert Schulze <robert_schulze@posteo.de>
2019-01-29 10:58:31 +0100

    working versions of functions task

--- 18-19-algorithmen_und_programmierung/functions/functions.c
+++ 18-19-algorithmen_und_programmierung/functions/functions.c
@@ -1,43 +1,35 @@
 //implements the functions necessary for task 005
 
 //sum of all numbers 1 to n
-unsigned int sum(unsigned int n)
-{
-  if(n != 0)
-  return n + sum(n-1);
-  else return n;
+unsigned int sum(unsigned int n) {
+  for(unsigned i = n-1; i > 0; i--)
+    n += i;
+  return n; 
 }
 
-//sum of first odd numbers in n
-unsigned int sum0dd(unsigned int n)
-{
-  if(n != 0 && n % 2 == 1)
-  return n + sum0dd(n-1);
-  else if(n != 0 && n % 2 == 0)
-  return n-1;
-  else return n;
+//sum of odd number from 1 to n
+unsigned int sum0dd(unsigned int n) {
+  for(unsigned i = n-1; i > 0; i--)
+    if(i % 2 == 1)
+      n += i;
+  return n;
 }
 
 
 //number of digits of n
-unsigned int digits(unsigned int n)
-{
+unsigned int digits(unsigned int n) {
   unsigned int count = 0;
-  while (n != 0)
-  {
+  while (n != 0) {
   n /= 10;
   ++count;
-  
   }
   return count;
 }
 
 //checksum of n
-unsigned int checksum(unsigned int n)
-{
+unsigned int checksum(unsigned int n) {
   int check = 0;
-  while(n != 0)
-  {
+  while(n != 0) {
     check += n % 10;
     n /= 10;
   }
--- 18-19-algorithmen_und_programmierung/functions/functiontest.c
+++ 18-19-algorithmen_und_programmierung/functions/functiontest.c
@@ -2,25 +2,17 @@
 
 #include <stdio.h>
 
-//function declaration - aka function call in python
-//unsigned int sum(unsigned int n);
-//unsigned int sum0dd(unsigned int n);
-//unsigned int digits(unsigned int n);
-//unsigned int checksum(unsigned int n);
-unsigned long int squareroot (unsigned long int val);
 
-int main()
-{
-  unsigned long int val = 16;
-  //unsigned int n;
-  //printf("Please enter a positive integer: ");
-  //scanf('%d', &n);                              //scanf causes errors atm b/c of pointers
-  //printf("sum(%d) = %d\n", n, sum(n));
-  //printf("sum0dd(%d)= %d\n", '3', sum0dd(3));
-  //printf("digits(%d) = %d\n", "24", digits(24));
-  //printf("checksum(%d) = %d\n", "24", checksum(24));
-  printf("The squareroot of %d is %d\n", val, squareroot(val));
-  return 0;
-  
+unsigned int sum(unsigned int n);
+unsigned int sum0dd(unsigned int n);
+unsigned int digits(unsigned int n);
+unsigned int checksum(unsigned int n);
+
+int main() {
+  unsigned int val = 5;
+  printf("sum(%d) = %d\n", val, sum(val));
+  printf("sum0dd(%d)= %d\n", val, sum0dd(val));
+  printf("digits(%d) = %d\n", val, digits(val));
+  printf("checksum(%d) = %d\n", val, checksum(val)); 
   return 0;
   }
