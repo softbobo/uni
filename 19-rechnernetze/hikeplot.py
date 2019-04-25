fcf18dac92ab22286f9179098f504a518f6f4894
Robert Schulze <robert_schulze@posteo.de>
2019-04-25 19:05:44 +0200

    initial commit

---
+++ 19-rechnernetze/hikeplot.py
@@ -0,0 +1,21 @@
+# written by Robert Schulze 25th April 2019
+
+""" First lab assignment for networks. Set as follows:
+    - take gps data from a file, it contains date, time, lat, lon and height
+    - create output file that contains:
+        - index
+        - distance to preceding point
+        - total distance covered 
+        - time difference to preceding point
+        - total time
+        - pace/speed for last section covered
+        - height above sea level
+        - lat
+        - lon
+    - plot a profile graph with gnuplot (distance on x- height on y-axes)
+""" 
+
+""" Algo:
+    - open file
+
+""" 
