1b0125568bc3c6ae210f7b94d463342cf9f22a11
Robert Schulze <robert_schulze@posteo.de>
2019-04-26 10:27:40 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_1-erassieve.cpp
@@ -0,0 +1,20 @@
+//Robert Schulze 26 April 2019
+
+/*
+First lab assignment for the data structures lecture. Task specified in pdf:
+a)  - write a function, that takes 'n' as an arg 
+    - returns count of primes in the interval [2,n] - 'p'
+    - also returns 's' - count of stroke numbers
+b)  - put that into a program (with main() and stuff) that takes 2^i (i <= 20)
+    and gives that to the function a)
+    - prints out the return values of the a) function right-bound, newline per val
+c)  - plot the whole thing: graph of p and s in correlation to n (make pdf)
+d)  - find a function that finds the minimal upper border in time complexity
+    of function a)
+*/
+
+#include<iostream>
+
+int main() {
+    
+}
