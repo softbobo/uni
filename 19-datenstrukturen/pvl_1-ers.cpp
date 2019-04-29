693b849ec9d365fb363ba62da9385be568eaf16e
Robert Schulze <robert_schulze@posteo.de>
2019-04-29 11:29:41 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_1-ers.cpp
@@ -0,0 +1,10 @@
+//Robert Schulze; 29. Apr 2019; Matrikelnummer 555625
+
+/* new try for first Data Structures lab assignment,
+contains mostly recycled code from pvl_1-erassieve.cpp */
+
+#include<iomanip>
+
+int main() {
+    
+}
