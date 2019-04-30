381883cb2ff6d237865925c38d205b9a87aaf98a
Robert Schulze <robert_schulze@posteo.de>
2019-04-30 11:50:44 +0200

    initial commit, set ouput to pdf

---
+++ 19-datenstrukturen/pvl1_ers_plot.sh
@@ -0,0 +1,11 @@
+# softbobo 30 Apr 2019
+# simple plotting script for the first lab assignment for data structures
+
+#!/usr/bin gnuplot
+
+set grid
+
+
+# set the correct output
+set term pdfcairo
+set output "pvl_1-ers.pdf"
