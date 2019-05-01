d35967cf60d6dd90a69f7c3f273088983bf53078
Robert Schulze <robert_schulze@posteo.de>
2019-05-01 10:54:38 +0200

    put output lines to the very front - bam! pdf readable, but very malformatted

--- 19-datenstrukturen/pvl_1-ers_plot.sh
+++ 19-datenstrukturen/pvl_1-ers_plot.sh
@@ -2,6 +2,10 @@
 # softbobo 30 Apr 2019
 # simple plotting script for the first lab assignment for data structures
 
+# set the correct output 
+set term pdf
+set output "pvl_1-ers.pdf"
+
 # set the layout of the plot
 set multiplot                       # enter multiplot mode
 set title "Sieb des Erastosthenes - P und S in Abhängigkeit von Eingangsgröße N"
@@ -15,6 +19,3 @@ plot "<(sed -n '2,20p' ers.txt)" using 1:2 with lines # first plot for number of
 plot "<(sed -n '2,20p' ers.txt)" using 1:3 with lines # second plot number of strokes
 unset multiplot                     # exit multiplot mode in order to display the output
 
-# set the correct output
-set term pdfcairo
-set output "pvl_1-ers.pdf"
