7816840a3a0d511461c20afa53d6b923452bfa4b
Robert Schulze <robert_schulze@posteo.de>
2019-05-01 10:18:02 +0200

    added file input, does output pdf but reader says isnt readable

--- 19-datenstrukturen/pvl_1-ers_plot.sh
+++ 19-datenstrukturen/pvl_1-ers_plot.sh
@@ -1,21 +1,20 @@
+#!/usr/bin/gnuplot
 # softbobo 30 Apr 2019
 # simple plotting script for the first lab assignment for data structures
-
-#!/usr/bin/gnuplot
 
 # set the layout of the plot
 set multiplot                       # enter multiplot mode
 set title "Sieb des Erastosthenes - P und S in Abhängigkeit von Eingangsgröße N"
 set grid
+set logscale xy                     # set both scales logarithmic
 set xlabel "N (obere Grenze im Intervall [2, N]"
 set ylabel ""
-set logscale xy                     # set both scales logarithmic
-
-unset multiplot                     # exit multiplot mode in order to display the output
 
 # specify the input
-
+plot "<(sed -n '2,20p' ers.txt)" using 1:2 with lines # first plot for number of primes
+plot "<(sed -n '2,20p' ers.txt)" using 1:3 with lines # second plot number of strokes
+unset multiplot                     # exit multiplot mode in order to display the output
 
 # set the correct output
-# set term pdfcairo
-# set output "pvl_1-ers.pdf"
+set term pdfcairo
+set output "pvl_1-ers.pdf"
