1eabc8a7be3c7121b4b2e6dd91e80a1db943b37e
Robert Schulze <robert_schulze@posteo.de>
2019-05-01 11:09:38 +0200

    graphs now in different plots, axes get proper naming

--- 19-datenstrukturen/pvl_1-ers_plot.sh
+++ 19-datenstrukturen/pvl_1-ers_plot.sh
@@ -2,20 +2,26 @@
 # softbobo 30 Apr 2019
 # simple plotting script for the first lab assignment for data structures
 
-# set the correct output 
+# set the correct output - at the beginning - super important
 set term pdf
 set output "pvl_1-ers.pdf"
 
 # set the layout of the plot
-set multiplot                       # enter multiplot mode
+set multiplot layout 2,1 rowsfirst                       # enter multiplot mode
 set title "Sieb des Erastosthenes - P und S in Abhängigkeit von Eingangsgröße N"
 set grid
 set logscale xy                     # set both scales logarithmic
-set xlabel "N (obere Grenze im Intervall [2, N]"
-set ylabel ""
+
+# --- plot a for the number of primes 'p' in interval [2,n]
+set xlabel "N (obere Grenze im Intervall [2, N])"
+set ylabel "P (Anzahl der Primzahlen im Intervall [2,N])"
+plot "<(sed -n '2,20p' ers.txt)" using 1:2 with lines # first plot for number of primes
+
+# --- plot b for the number of strikestroughs 's' needed to calculate 'p' 
+set xlabel "N (obere Grenze im Intervall [2, N])"
+set ylabel "S (Anzahl der Streichungen im Sieb)"
+plot "<(sed -n '2,20p' ers.txt)" using 1:3 with lines # second plot number of strokes
 
 # specify the input
-plot "<(sed -n '2,20p' ers.txt)" using 1:2 with lines # first plot for number of primes
-plot "<(sed -n '2,20p' ers.txt)" using 1:3 with lines # second plot number of strokes
 unset multiplot                     # exit multiplot mode in order to display the output
 
