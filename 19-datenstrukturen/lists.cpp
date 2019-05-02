8bdba57ed71f489a67ca70d234a2e2fb81b4717d
Robert Schulze <robert_schulze@posteo.de>
2019-05-02 14:04:10 +0200

    initial commit

---
+++ 19-datenstrukturen/lists.cpp
@@ -0,0 +1,18 @@
+//data structures lab 02 May 2019, on lists
+//softbobo
+
+//task: write a function that allocates a list of arbitrary length
+
+#include<iostream>
+
+using namespace std; 
+
+struct node{
+    int data;
+    node* next;
+};
+
+
+void writeList(node* head) {
+
+}
