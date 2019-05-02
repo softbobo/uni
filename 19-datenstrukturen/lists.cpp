ffa570b9819637cff9907cefab6792c433f84646
Robert Schulze <robert_schulze@posteo.de>
2019-05-02 14:57:56 +0200

    added various solutions for searching, inserting, printing out singly linked list

--- 19-datenstrukturen/lists.cpp
+++ 19-datenstrukturen/lists.cpp
@@ -1,18 +1,60 @@
 //data structures lab 02 May 2019, on lists
 //softbobo
 
-//task: write a function that allocates a list of arbitrary length
+
 
 #include<iostream>
 
 using namespace std; 
 
 struct node{
-    int data;
-    node* next;
+    int data = 0;
+    node* next = NULL;
 };
 
-
+//task: write a function that prints contents of a list of arbitrary length
+//preferably recursive
 void writeList(node* head) {
+    if(head != NULL) {
+        cout << head->data << endl;
+        writeList(head->next);
+    }
+}
 
+//task: insert element at list head
+node* insertAtHead(node* head, int val) {
+    node* temp = new node;
+    temp->next = head;
+    temp->data = val;
+    head = temp;
+    return head;
+}
+
+//task: insert element at list end
+void insertAtTail(node* head, int val) {
+    if(head == NULL) {
+        //new node, fill with data 'n stuff
+        node* temp = new node;
+        temp->next = NULL;
+        temp->data = val;
+        head = temp;
+
+        return;
+    }
+    return insertAtTail(head->next, val);
+}
+
+//searches for list entry with given value iteratively
+node* iterSearch(node* head, int val) {
+    while(head->next != NULL) {
+        if(head->data == val) { return head; }
+    }
+    return NULL;
+}
+
+//search for list entry recursively
+node* recurSearch(node* head, int val) {
+    if(head == NULL) { return 0; }
+    if(head->data == val) { return head; }
+    return recurSearch(head->next, val);
 }
