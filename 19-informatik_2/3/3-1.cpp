adf73d59cf0b5fa2419345e4d29b41ced33fa612
Robert Schulze <robert_schulze@posteo.de>
2019-05-03 17:09:11 +0200

    added iterative implementation, not tested yet

--- 19-informatik_2/3/3-1.cpp
+++ 19-informatik_2/3/3-1.cpp
@@ -4,9 +4,18 @@ first task in block three, implement function that prints linked list
 softbobo 03 may 2019
  */
 
+using namespace std;
+
 #include<iostream>
 #include"3.h"
 
 void ll_print(lelem* head) {
-
+    if(head == NULL) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    while(head != NULL) { 
+        cout << head->data << endl;
+        head = head->next;
+        }
 }
