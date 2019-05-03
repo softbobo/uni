54e8dd5649a344793f34062f7d1e8253b4216754
Robert Schulze <robert_schulze@posteo.de>
2019-05-03 16:57:00 +0200

    inital commit

---
+++ 19-informatik_2/3/3-main.cpp
@@ -0,0 +1,12 @@
+/* 
+Main function for the whole task block three
+
+softbobo 03 May 2019
+*/
+
+using namespace std;
+
+
+int main() {
+    return 0;
+}
---
+++ 19-informatik_2/3/3.h
@@ -0,0 +1,12 @@
+/*
+header file for the whole lab task block number three
+
+Softbobo
+03 May 2019
+*/
+
+/* this one was given, didn't think of the name myself */
+struct lelem {
+    int data;
+    lelem* next;
+};
