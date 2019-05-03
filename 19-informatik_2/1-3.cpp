2b3dea78b9e31aaadc943584d08f85be207d5608
Robert Schulze <robert_schulze@posteo.de>
2019-05-03 15:46:35 +0200

    initial commit

---
+++ 19-informatik_2/1-3.cpp
@@ -0,0 +1,19 @@
+/* 
+implementation of task 1.3: write a function that prints whole array using
+pointers exclusively (function signature is given)
+
+Robert Schulze, 03 May 2019
+*/
+
+using namespace std;
+
+#include<iostream>
+
+int main() {
+    return 0;
+}
+
+/*  */ 
+void print(int* array, int len) {
+
+}
