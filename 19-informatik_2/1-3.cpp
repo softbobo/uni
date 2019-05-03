e22d9c3b7c95ff6cf00c7d697f2fb5a38e2c1666
Robert Schulze <robert_schulze@posteo.de>
2019-05-03 16:08:33 +0200

    solution added, learned again, that ++ and -- dont work in function calls

--- 19-informatik_2/1-3.cpp
+++ 19-informatik_2/1-3.cpp
@@ -9,11 +9,27 @@ using namespace std;
 
 #include<iostream>
 
+/* recursive implementation, to not forget how to do it */ 
+void print(int* array, int len) {
+    if(len > 0) {
+        cout << *array << " " << len << endl;
+        return print(array+1, len - 1);
+    }
+
+    return;
+}
+
 int main() {
+    /* declare the data needed */
+    int len = 10;
+    int array[len];
+    
+    /* initialize the array */
+    for(int i = 0; i < len; i++) { array[i] = i; }
+    
+    /* call function */
+    print(array, len);
+
     return 0;
 }
 
-/*  */ 
-void print(int* array, int len) {
-
-}
