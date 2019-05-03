253ec7665416a0dd74a4d2169de5f55cddb36739
Robert Schulze <robert_schulze@posteo.de>
2019-05-03 21:36:08 +0200

    implemented function, not tested yet, too tired

---
+++ 19-informatik_2/3/3-7.cpp
@@ -0,0 +1,21 @@
+/*
+write a function, that removes the first element of the list
+
+softbobo 03 May 2019
+*/
+
+using namespace std;
+
+#include"3.h"
+#include<iostream>
+
+void ll_remove_front(lelem* &head, unsigned len) {
+    if(len == 0) {
+        cout << "list is empty" << endl;
+        return;
+    }
+
+    lelem* temp = head->next;
+    delete head;
+    head = &temp;
+}
