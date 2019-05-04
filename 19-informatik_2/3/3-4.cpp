49630ed8e5e2cceb3ec5252f60b7b41e77ac2bf3
Robert Schulze <robert_schulze@posteo.de>
2019-05-04 11:52:22 +0200

    actual initial commit

---
+++ 19-informatik_2/3/3-4.cpp
@@ -0,0 +1,16 @@
+/*
+task: write a function that calculates the relation of negative
+elements in the list and gives back a a float < 1.0 representing the 
+actual percentage (via variable * 100 = percentage)
+
+softbobo 04 May 2019
+*/
+
+using namespace std;
+
+#include"3.h"
+
+float ll_negrelation(lelem* head) {
+    
+}
+
