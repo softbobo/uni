736461e782fb7844451981ba143a7b32ed9b5d06
Robert Schulze <robert_schulze@posteo.de>
2019-05-04 11:31:48 +0200

    initial commit

---
+++ 19-informatik_2/3/3-2.cpp
@@ -0,0 +1,13 @@
+/*
+task: write a function that gives back the number of elementsof the list
+
+softbobo 04 May 2019
+*/
+
+using namespace std;
+
+#include"3.h"
+
+unsigned ll_size(lelem* head) {
+
+}
