80691111c5bc67b3ac36108c3b5bb502674dd9e6
Robert Schulze <robert_schulze@posteo.de>
2019-05-06 15:08:42 +0200

    added implementation, not tested yet

--- 19-informatik_2/3/3-13.cpp
+++ 19-informatik_2/3/3-13.cpp
@@ -9,5 +9,15 @@ using namespace std;
 #include"3.h"
 
 bool ll_remove_data(lelem* head, int val) {
-    
+    lelem* previous = head;
+
+    while(head->next != NULL) {
+        if(head->data == val) {
+            previous = head->next;
+            ll_remove_front(head);
+            return true;
+        }
+    }
+
+    return false;
 }
