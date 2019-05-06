f6a4c97cc403f29551edd28fb0b4fd7715d81592
Robert Schulze <robert_schulze@posteo.de>
2019-05-06 13:54:49 +0200

    initial commit

---
+++ 19-informatik_2/3/3-16.cpp
@@ -0,0 +1,13 @@
+/*
+task: write a recursive function, that puts out all list elements
+
+softbobo 06 May 2019
+*/
+
+using namespace std;
+
+#include"3.h"
+
+void ll_print_list(lelem* &head) {
+
+}
