6e5c103a461c89ed1794bed985ae27bfd5cdc641
Robert Schulze <robert_schulze@posteo.de>
2019-05-06 09:32:22 +0200

    added implementation for inserting at the back, not tested yet

--- 19-informatik_2/3/3-6.cpp
+++ 19-informatik_2/3/3-6.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 #include"3.h"
 
-void ll_insert_back() {
+/* so this is a pretty dumb way to insert somethin at the back of the list,
+i know. but i am too lazy atm to implement a tail pointer - maybe later */
+void ll_insert_back(lelem* &head, int data) {
+    while(head->next != NULL) {
+        head = head->next;
+    }
 
+    lelem* temp = new lelem;
+    temp->data = data;
+    head->next = temp;
 }
