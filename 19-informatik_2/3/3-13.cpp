89e385ab5ea29ba4c06eee6d33e22670518bd781
Robert Schulze <robert_schulze@posteo.de>
2019-05-06 14:42:44 +0200

    initial commit

---
+++ 19-informatik_2/3/3-13.cpp
@@ -0,0 +1,13 @@
+/*
+task: write a function that that removes the first entry holding the specified value
+
+softbobo 06 May 2019
+*/
+
+using namespace std;
+
+#include"3.h"
+
+bool ll_remove_data(lelem* head, int val) {
+    
+}
