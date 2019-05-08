13bfd24df3f14d95c601d8bf1a218cc36791ce52
Robert Schulze <robert_schulze@posteo.de>
2019-05-08 13:56:31 +0200

    wrote distdiff function, changed output function b/c index is now part of the dataset argument, startet writing whole logic

--- 19-rechnernetze/1/hikeplot.py
+++ 19-rechnernetze/1/hikeplot.py
@@ -29,8 +29,7 @@
 - organize into application logic
 """
 
-import cmath as cm
-import os
+import math
 import datetime as dt
 
 #basic file i/o
@@ -41,23 +40,46 @@ outfile = open('output.plt', 'w')
 index = 0
 
 #calculations
-def calc_time(time_prev, time_act):
+def timediff(time_prev, time_act):
     return dt.timedelta(time_prev, time_act)
 
-
+# this function is ugly and surely can be optimized
+# taken from the task however
+def distdiff(lat_prev, lon_prev, lat_act, lon_act):
+    lat_prev = math.radians(lat_prev)
+    lon_prev = math.radians(lon_prev)
+    lat_act = math.radians(lat_act)
+    lon_act = math.radians(lon_act)
+    latdiff = lat_act - lat_prev
+    londiff = lon_act - lon_prev
+    a = math.sin(latdiff/2.0)**2 + math.cos(lat_prev) * math.cos(lat_act) * math.sin(londiff/2.0)**2
+    b = 2.0 * math.asin(min(1, math.sqrt(a)))
+    return 6396.0 * b 
+    
 
 # ouput
-def write_to_file(dataset, index):
-    outfile.write(str(index))
-    outfile.write('\t')
+def write_to_file(dataset):
     for item in dataset:
         outfile.write(item)
         outfile.write('\t')
     outfile.write('\n')
 
+index = 0
+
+# this is the whole logic in a single for loop - urgh
 for line in infile: 
+    
+    # empty list to fill with data and pass to write_to_file in the end
+    dataset = []
+
+    # leave out lines without actual data
     if not '\t' in line:
         continue
+    
+    # write index to dataset 
+    dataset.append(index)
+    
+    # parse input
     sline = line.split('\t')
     time = str(sline[1]).split()
     time = time[1]
@@ -66,8 +88,9 @@ for line in infile:
     height = sline[4].replace('\n', '')
     dataset = [time, lat, lon, height]
     write_to_file(dataset, index)
-    index += 1
 
+    # increment index right away for the next line
+    index += 1
 
 
 
