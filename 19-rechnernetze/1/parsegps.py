ab28601e5c118377b489b5f180a8ceab71c74dad
Robert Schulze <robert_schulze@posteo.de>
2019-05-10 09:39:49 +0200

    set up basic script structure

--- 19-rechnernetze/1/parsegps.py
+++ 19-rechnernetze/1/parsegps.py
@@ -1,4 +1,15 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 # Robert Schulze
 # Matrikelnummer: 555625
 # 09 Mai 2019
+
+# '*' is going to be changed to discrete module names later  
+from parsegps_modules import *
+
+
+def main():
+    """ script launcher """
+    pass
+
+if __name__ == "__main__":
+    main()
