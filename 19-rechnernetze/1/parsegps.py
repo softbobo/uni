d770a00740ca23959d783b1d3a2fa8144fd70650
Robert Schulze <robert_schulze@posteo.de>
2019-05-10 10:19:59 +0200

    added index variable and write_to_file, also for loop for input file

--- 19-rechnernetze/1/parsegps.py
+++ 19-rechnernetze/1/parsegps.py
@@ -4,11 +4,31 @@
 # 09 Mai 2019
 
 # '*' is going to be changed to discrete module names later  
-from parsegps_modules import *
+from parsegps_modules import instance
+
+""" as the name says, this is the logic of the script mainly;
+it sets a new instance and calls its member functions """
+def logic():
+    infile = open('data.txt', 'r')
+    outfile = open ('output.plt', 'w')
+
+    data = instance()
+
+    for line in infile:
+        if '\t' not in line:
+            continue
+        
+        line = line.split('\t')
+
+        data.write_to_file(data.index, outfile)
+        data.index += 1
+
+        outfile.write('\n')
 
 
 def main():
     """ script launcher """
+    logic()
     pass
 
 if __name__ == "__main__":
--- 19-rechnernetze/1/parsegps_modules.py
+++ 19-rechnernetze/1/parsegps_modules.py
@@ -1,3 +1,13 @@
 # Robert Schulze
 # Matrikelnummer: 555625
 # 09 Mai 2019
+
+class instance():
+    index = 0
+    def __init__(self):
+        pass
+
+    
+    def write_to_file(self, argument, outfile):
+        outfile.write(str(argument))
+        outfile.write('\t\t')
