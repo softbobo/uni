4e589f54d1f809f9b103b105eb68f381d4f40d8e
Robert Schulze <robert_schulze@posteo.de>
2019-05-11 12:17:53 +0200

    added total distance calculation, changed formula for sexages-to-dec conversion, changed whole algo for distance calculation

--- 19-rechnernetze/1/parsegps.py
+++ 19-rechnernetze/1/parsegps.py
@@ -3,7 +3,13 @@
 # Matrikelnummer: 555625
 # 09 Mai 2019
 
-# '*' is going to be changed to discrete module names later  
+"""
+next steps:
+- fix section distance output for first value (needs to be 0.0)
+- fix distance formatting for distance outputs
+- write lat and lon to file
+"""
+
 from parsegps_modules import instance
 
 """ as the name says, this is the logic of the script mainly;
@@ -19,11 +25,28 @@ def logic():
             continue
         
         line = line.split('\t')
-
+            
         data.write_to_file(data.index, outfile)
         data.index += 1
 
-        outfile.write('\n')
+        if data.index is not 0:
+            data.lat_prev = data.lat
+            data.lon_prev = data.lon
+        data.lat = line[2]
+        data.lon = line[3]
+        height = line[4]
+
+        distance_section = data.calc_distance(data.lat, data.lon, data.lat_prev, data.lon_prev)
+        data.write_to_file(distance_section, outfile)
+
+        data.tot_distance += distance_section
+        data.write_to_file(data.tot_distance, outfile)
+
+        
+        data.write_to_file(data.lat, outfile)
+        data.write_to_file(data.lon, outfile)
+        data.write_to_file(height, outfile)
+        
 
 
 def main():
--- 19-rechnernetze/1/parsegps_modules.py
+++ 19-rechnernetze/1/parsegps_modules.py
@@ -2,12 +2,46 @@
 # Matrikelnummer: 555625
 # 09 Mai 2019
 
+from math import sin, cos, acos, sqrt
+
 class instance():
     index = 0
+    lat = "0"
+    lon = "0"
+    lat_prev = "0"
+    lon_prev = "0"
+    tot_distance = 0.0
+
+
     def __init__(self):
         pass
 
-    
+    """
+    converts a gps input string in sexagesimal format to decimal degrees
+    """
+    def sexages_to_dec(self, coord):    
+        if coord != '0':
+            coord = coord.split()
+            return (float(coord[0]) + float(coord[1]) / 60 + float(coord[2]) / 3600)
+        else:
+            return float(coord)
+
+    """ 
+    as the name says, cals the distance between two points on earth, thinking of
+    it as an idealized sphere with 40.000km circumference
+    now, this implementation and formula varies from the given one (see task), 
+    but i just couldn't get my head around this one without implementation
+    """ 
+    def calc_distance(self, lat, lon, lat_prev, lon_prev):
+        lat = self.sexages_to_dec(lat)
+        lon = self.sexages_to_dec(lon)
+        lat_prev = self.sexages_to_dec(lat_prev)
+        lon_prev = self.sexages_to_dec(lon_prev)
+        zeta = acos(sin(lat)*sin(lat_prev) + cos(lat)*cos(lat_prev)*cos(lon_prev - lon))
+        distance = (zeta/360) * 40000
+        return distance
+
+
     def write_to_file(self, argument, outfile):
         outfile.write(str(argument))
         outfile.write('\t\t')
