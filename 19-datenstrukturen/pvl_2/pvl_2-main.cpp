e04c0227eca449d3655516520f502427e2ed0ccb
Robert Schulze <robert_schulze@posteo.de>
2019-05-12 17:41:36 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_2/pvl_2-main.cpp
@@ -0,0 +1,30 @@
+/*
+Robert Schulze
+Matrikelnummer: 555625
+May 2019
+*/
+
+/* Second lab assignment for data structures. Tasks:
+- design a data structure w/ an array of pointer [1000] pointing to a structure
+w/ unique number and courses (30 max - array of strings) of students
+- write an input function for student data
+- function, that finds a student with given number, bonus points for solutions
+faster than O(n)
+- function, that deletes a student's entry, musn't leave null pointers in between
+entries
+- interface with options to choose from aforem functions
+
+important:
+- strings must use only as much space as necessary
+- delete[] allocated memory
+- libfuncs only for input and output
+*/
+
+using namespace std;
+
+#include<iostream>
+#include"pvl_2.h"
+
+int main() {
+
+}
---
+++ 19-datenstrukturen/pvl_2/pvl_2.h
@@ -0,0 +1,18 @@
+/*
+Robert Schulze
+Matrikelnummer: 555625
+May 2019
+*/
+
+/* header for second lab assignment */
+
+#ifndef PVL_2_H
+#define PVL_2_H
+
+class pvl2_students {
+
+public:
+
+};
+
+#endif
