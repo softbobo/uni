da84b6bb43c9c355f69ef15e695eb80b42f1be43
Robert Schulze <robert_schulze@posteo.de>
2019-05-12 17:57:11 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_2/pvl_2-students.cpp
@@ -0,0 +1,14 @@
+/*
+Robert Schulze
+Matrikelnummer: 555625
+May 2019
+*/
+
+/* functions declarations for 2nd lab assignment */
+
+/* to do:
+- which namespace to use?
+- implement pvl_2-add_student
+*/
+
+#include"pvl_2.h"
