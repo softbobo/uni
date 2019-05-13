de5b367f41c0094b666c6b502d9e178e5bf36717
Robert Schulze <robert_schulze@posteo.de>
2019-05-13 15:32:55 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_2/pvl_2-menu.cpp
@@ -0,0 +1,15 @@
+/*
+Robert Schulze
+Matrikelnummer: 555625
+May 2019
+*/
+
+/* menu implementation to control function execution for part e) */
+
+#include"pvl_2-menu.h"
+#include"pvl_2.h"
+
+void pvl_2_menu() {
+
+    return;
+}
---
+++ 19-datenstrukturen/pvl_2/pvl_2-menu.h
@@ -0,0 +1,9 @@
+/*
+Robert Schulze
+Matrikelnummer: 555625
+May 2019
+*/
+
+/* header file for menu implementation */
+
+void pvl_2_menu();
