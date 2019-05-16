5ff9aede34fc03b6d546dc85399123a63ea063b7
Robert Schulze <robert_schulze@posteo.de>
2019-05-16 15:01:05 +0200

    initial commit complete w/ all implementations

---
+++ 19-datenstrukturen/190516-adt.cpp
@@ -0,0 +1,107 @@
+//data structures lab 16th may 2019
+//softbobo
+
+/* implementation of a doubly linkes list as an ADT */
+
+using namespace std;
+
+#include<iostream>
+
+class List {
+
+    struct node {
+        node* next;
+        node* prev;
+        int data;
+    };
+    node* head;
+    node* tail;
+
+    public:
+        
+        List();                             //constructor
+        ~List();                            //destructor
+        void print_list();
+        void insert_node(int value);
+        void remove_node(int value);
+        bool node_exists(int value);
+        int count();
+};
+
+List::List() {
+    head = NULL;
+    tail = NULL;
+}
+
+List::~List() {
+    if(head) {
+        while(head->next) {
+            head = head->next;
+            delete head->prev;
+        }
+        delete head;
+    }
+}
+
+void List::print_list() {
+        node* temp = head;
+        while(temp){
+            cout << temp->data << endl;
+            temp = temp->next;
+    }
+}
+
+void List::insert_node(int value) {
+    node* temp = new node;
+    temp->data = value;
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+
+void List::remove_node(int value) {
+    node* temp = head;
+
+    while(temp) {
+        if(temp->data = value) {
+            if(!temp->prev) {
+                temp->next->prev = NULL;
+                head = temp->next;
+                delete temp;
+            }
+            else if(!temp->next) {
+                temp->prev->next = NULL;
+                tail = temp->prev;
+                delete temp;
+            }
+            else {
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+                delete temp;
+            }
+        }
+    }
+}
+
+bool List::node_exists(int value) {
+    if(head) {
+        node* temp = head;
+        while(temp) {
+            if(temp->data) { return true; }
+        }
+    }
+    return false;
+}
+
+int List::count() {
+    int count = 0;
+    if(head) {
+        node* temp = head;
+        while(temp) {
+            count += 1;
+            temp = temp->next;
+        }
+    }
+
+    return count;
+}
