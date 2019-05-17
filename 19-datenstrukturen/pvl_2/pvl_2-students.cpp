2c94eafa70f21462271648975ebadab3a17e242c
Robert Schulze <robert_schulze@posteo.de>
2019-05-17 19:16:51 +0200

    added implementation for search_entry(), not tested

--- 19-datenstrukturen/pvl_2/pvl_2-students.cpp
+++ 19-datenstrukturen/pvl_2/pvl_2-students.cpp
@@ -32,13 +32,67 @@ pvl2_students::pvl2_students() {
 void pvl2_students::pvl2_add_entry() {
     stud* temp = new stud;
         
-    /* first copy data to new entry */
+    /* first copy regnum to new entry */
     unsigned regnum = 0;
     cout << "Bitte Matrikelnummer des Studierenden eingeben: ";
     cin >> regnum;
+    temp->regnum = pvl2_validity_check(regnum);
+
+    /* next are courses */
+
+
 }
 
-void pvl2_students::pvl2_validity_check(unsigned regnum) {
+unsigned pvl2_students::pvl2_validity_check(unsigned regnum) {
+
+    /* check, if regnum is within bounds first */
+    if(regnum < 1 || regnum > 999999) {
+        unsigned new_regnum = 0;
+        
+        cout << "Unzulaessige Eingabe - Matrikelnummer muss sich zwischen 1 " <<
+        "und 999999 befinden!" << endl;
+        cout << "Bitte erneut eingeben." << endl;
+
+        cin >> new_regnum;
+        return pvl2_validity_check(new_regnum);
+    }
+
+    /* next check if regnum is already taken */
+    else if(pvl2_search_entry(regnum, 0, count)) {
+        unsigned new_regnum = 0;
+        cout << "Unzulaessige Eingabe - Matrikelnummer ist bereits vergeben." << endl;
+        cout << "Bitte erneut eingeben." << endl;
+
+        cin >> new_regnum;
+        return pvl2_validity_check(new_regnum);
+    }
+
+    /* if regnum is OK, return it to add_entry() */
+    return regnum;
 
 
+}
+
+/* recursive binary search through the array */
+struct stud* pvl2_students::pvl2_search_entry(unsigned regnum, unsigned start, unsigned end) {
+    
+    /* no matching value found */
+    if(end < start) { return NULL; }
+    
+    unsigned middle = ((start + end) / 2);
+
+    if(registry[middle]->regnum == regnum) {
+        return registry[middle];
+    }
+    else if(registry[middle]->regnum <= regnum) {
+        return pvl2_search_entry(regnum, middle + 1, end);
+    }
+    else if(registry[middle]->regnum >= regnum) {
+        return pvl2_search_entry(regnum, start, middle - 1);
+    }
+
+}
+
+void pvl2_students::pvl2_add_courses(stud* entry) {
+    
 }
