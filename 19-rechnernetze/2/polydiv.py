2a082172256a999a5fd45e15d9cc0861472e4151
Robert Schulze <robert_schulze@posteo.de>
2019-05-27 14:11:52 +0200

    enhanced comments, implemented input function

--- 19-rechnernetze/2/polydiv.py
+++ 19-rechnernetze/2/polydiv.py
@@ -1,4 +1,5 @@
 #!usr/bin/env python2
+# coding=utf8
 
 # Robert Schulze
 # Matrikelnummer: 555625
@@ -6,13 +7,25 @@
 
 """ Task is:
 Write a script that puts out the modulo of a polynomial division of a string
-put in from the terminal and the term x**4+x+1 """
+put in from the terminal and the term x**4+x+1 
 
+what the script needs to do:
+- take the string from the CLI (what about exceptions)?
+- split the input string into a list
+- perform division on that list (horner scheme maybe?)
+- make output nice
+"""
+
+from sys import stdin
+
+def polydiv_input():
+    line = raw_input("Bitte binäre Zeichenkette eingeben: ")
+    [line[i:i+1] for i in range(0, len(line), 1)]
+    print(line)                                                 # this is for debugging only
 
 
 def main():
     """ script launcher """
-    logic()
     pass
 
 if __name__ == "__main__":
