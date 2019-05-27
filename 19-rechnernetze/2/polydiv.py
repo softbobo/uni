f209ddaa2b86f13af29e426df780b325b58ad8a8
Robert Schulze <robert_schulze@posteo.de>
2019-05-27 13:28:39 +0200

    initial commit

---
+++ 19-rechnernetze/2/polydiv.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python2
+
+# Robert Schulze
+# Matrikelnummer: 555625
+# Mai 2019
+
+""" Task is:
+Write a script that puts out the modulo of a polynomial division of a string
+put in from the terminal and the term x**4+x+1 """
+
+
+
+def main():
+    """ script launcher """
+    logic()
+    pass
+
+if __name__ == "__main__":
+    main()
