c271a80df4c921b419798982cec3a7408a3bc3ab
Robert Schulze <robert_schulze@posteo.de>
2019-06-06 07:58:45 +0200

    initial commit

---
+++ 19-rechnernetze/3/mport.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+# coding=utf8
+
+# Robert Schulze
+# Matrikelnummer 555625
+
+""" Task
+Write a multiport bridge simulation as a Py script. Args come from 
+the CLI of course. Args are: Input port, sender IP and receiver IP.
+For simplicity adresses range from 1 to 255, 255 being the broadcast address.
+"""
+
+def main():
+    """ script launcher """
+    pass
+
+if __name__ == "__main__":
+    main()
