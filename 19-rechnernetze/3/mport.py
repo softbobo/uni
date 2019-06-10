c1267cae8aae7e3b6799c43e3026f7f5e8b7af5c
Robert Schulze <robert_schulze@posteo.de>
2019-06-10 13:58:59 +0200

    changed list keys to strings and execution logic to exit()-statement first, no while loop in prompt() anymore

--- 19-rechnernetze/3/mport.py
+++ 19-rechnernetze/3/mport.py
@@ -10,35 +10,37 @@ the CLI of course. Args are: Input port, sender IP and receiver IP.
 For simplicity adresses range from 1 to 255, 255 being the broadcast address.
 """
 
-def fill_sat(sat, data_in):
-    return
+#def fill_sat(sat, data_in):
+#    return
 
 def prompt(sat):
-    data_in = []
-    while data_in[0] != 'e':
-        data_in = raw_input("switch<: ")
-        
-        """print the whole sat, if input is 'a'"""
-        if data_in[0] == 'a':
-            for key, values in sat.iteritems():
-                print(key + ":" + values)
-        
-        else:
-            for key, values in sat.iteritems():
-                """output on all ports if target is broadcast or not yet known"""
-                if value != data_in[1] or data_in[2] == '255':
-                    print("switch> Ausgabe auf allen Ports")
-                    """add adress to sat, if it is not in it yet"""
-                    if value != data_in[1]:
-                        sat[data_in[0]].append[data_in[1]]
-                """print out the port if a value for given address is known"""
-                else:
-                    print("switch> Ausgabe auf Port " + key)
-
+    data_in = raw_input("switch<: ")
+    data_in = data_in.split()
+    # exit-statement
+    if data_in[0] == 'e':
+        print('Programm wird beendet.')
+        exit()
+    # print the whole sat, if input is 'a'
+    elif data_in[0] == 'a':
+        for key, values in sat.iteritems():
+            print(key + ":" + values)
+        return prompt(sat)
+    else:
+        for key, values in sat.iteritems():
+            """output on all ports if target is broadcast or not yet known"""
+            if values != data_in[1] or data_in[2] == '255':
+                print("switch> Ausgabe auf allen Ports")
+                """add adress to sat, if it is not in it yet"""
+                if values != data_in[1]:
+                    sat[data_in[0]].append[data_in[1]]
+            """print out the port if a values for given address is known"""
+            #else:
+            #    print("switch> Ausgabe auf Port " + key)
+        return prompt(sat)
 
 def main():
     """ script launcher """
-    sat = {1:[], 2:[], 3: [], 4:[], 5:[], 6:[]}                       # empty dict represensting all ports
+    sat = {'1':[], '2':[], '3':[], '4':[], '5':[], '6':[]}                       # empty dict represensting all ports
     print("Multiport Bridge Simulation. Steuerung: \
         Pakete verschicken: <Port> <Absenderadresse> <Zieladresse> \
         Ausgabe der Source-Adress-Table: a \
