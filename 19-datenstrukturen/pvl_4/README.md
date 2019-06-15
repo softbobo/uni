f52258acf84b9e568e457e74cd9b79de0601091a
Robert Schulze <robert_schulze@posteo.de>
2019-06-15 21:58:21 +0200

    initial commit

---
+++ 19-datenstrukturen/pvl_4/README.md
@@ -0,0 +1,7 @@
+### Controlflow:
+1. parse args into two variables
+2. call read-in function on both textfiles 
+
+### Errorchecks:
+1. check, if two args (+1) are given when starting program 
+2. 
---
+++ 19-datenstrukturen/pvl_4/pvl_4.cpp
@@ -0,0 +1,26 @@
+//Robert Schulze
+//Matrikelnummer 555625
+
+#include<iostream>
+
+using namespace std;
+
+/* takes one of the read-in filenames as parameter and returns array with 
+the vals for the matrices */
+int* pvl4_read_in(char* fname) {
+
+}
+
+int main(int argc, char** argv) {
+
+    if(argc != 3) {
+        cout << "Es müssen exakt zwei Dateinamen übergeben werden."
+        << "Bitte Programm erneut starten";
+        return 1;
+    }
+
+    int* vals_1 = pvl4_read_in(argv[1]);
+    int* vals_2 = pvl4_read_in(argv[2]); 
+
+    return 0;
+}
