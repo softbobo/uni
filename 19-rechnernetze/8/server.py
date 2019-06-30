9cf56a6dbc5844821127e1adc2574213d036a1dc
Robert Schulze <robert_schulze@posteo.de>
2019-06-30 13:25:34 +0200

    implemented calc(), rudimentary tests successful

--- 19-rechnernetze/8/server.py
+++ 19-rechnernetze/8/server.py
@@ -6,11 +6,26 @@
 
 """ Taks:
 implement a simple server, that takes a basic arithmetic operation and two
-numbers at port 8080 an outputs the result
+numbers at port 8080 an outputs the result pluss proper error handling
 """
- 
+
 import socket
 
+
+def calc(op, var_1, var_2):
+    """ does the calculations """
+    if op == "ADD":
+        return str(var_1 + var_2)
+    elif op == "SUB":
+        return str(var_1 - var_2)
+    elif op == "MUL":
+        return str(var_1 * var_2)
+    elif op == "DIV":
+        return str(var_1 / var_2)
+    else:
+        print "Unbekannte Operation!"
+        return prompt()
+    
 def main():
     """ script launcher """
     pass
