572aba6c95faad9dac20acc9e78e57a52b08ab99
Robert Schulze <robert_schulze@posteo.de>
2019-06-30 13:15:23 +0200

    initial commit

---
+++ 19-rechnernetze/8/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# coding=utf8
+
+# Robert Schulze
+# Matrikelnummer 555625
+
+""" Taks:
+implement a simple server, that takes a basic arithmetic operation and two
+numbers at port 8080 an outputs the result
+"""
+ 
+import socket
+
+def main():
+    """ script launcher """
+    pass
+
+if __name__ == "__main__":
+    main()
