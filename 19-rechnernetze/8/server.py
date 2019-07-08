5073d8a2be49f193db9a4da911298292fdb76366
Robert Schulze <robert_schulze@posteo.de>
2019-07-08 09:18:45 +0200

    introduced script-structure, does not properly close socket yet

--- 19-rechnernetze/8/server.py
+++ 19-rechnernetze/8/server.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python2
+# coding=utf8
+
+# Robert Schulze
+# Matrikelnummer 555625
+
 import socket
 
-def make_sock():
+def server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('localhost', 8080))
     sock.listen(1)
-    while 1:
+    while True:
         conn, addr = sock.accept()
         msg = conn.recv(1024)
         msg = msg.split('*')
-        if len(msg) > 2 or len(msg) < 2:
+        if len(msg) > 3 or len(msg) < 3:
+            conn.send(result)
+            conn.close()
+            sock.close()
             raise RuntimeError('Falsches Format')
         print('receiving message from IP %d, port %d: %s', addr[0], addr[1], \
-            [for item in msg print item])
-        result = calc(msg[0], msg[1], msg[2])
+            [item for item in msg])
+        result = calc(msg[0], float(msg[1]), float(msg[2]))
         conn.send(result)
         conn.close()
     sock.close()
@@ -28,4 +37,8 @@ def calc(op, var_1, var_2):
     elif op == "DIV":
         return str(var_1 / var_2)
     else:
-        raise RuntimeError('Unbekannte Operation!')
+        return "Unbekannte Operation!"
+
+if __name__ == "__main__":
+    server()
+    pass
