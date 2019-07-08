884707219ac39ce2c8260af06e2aefda3d9924bd
Robert Schulze <robert_schulze@posteo.de>
2019-07-08 09:17:47 +0200

    changed script-structure with header and __main__ call

--- 19-rechnernetze/8/client.py
+++ 19-rechnernetze/8/client.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python2
+# coding=utf8
+
+# Robert Schulze
+# Matrikelnummer 555625s
+
 import socket
 
 def client():
-    clsock = socket.socket(AF_INET, SOCK_STREAM)
-    clsock.connect('localhost', 12000)
+    clsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clsock.connect(("localhost", 8080))
     msg = raw_input("Bitte Nachricht im Format OPE*VAR*VAR eingeben: ")
     clsock.send(msg)
     msg = clsock.recv(1024)
-    print('Das Ergebnis lautet ' + msg)
+    print("Das Ergebnis lautet " + msg)
     clsock.close()
+
+if __name__ == "__main__":
+    client()
+    pass
